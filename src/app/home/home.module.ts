44c8c7fb6a15edd1a607c58df3d3ccd3a9e96705
Jos <jpkotze148@gmail.com>
2024-07-07 19:47:03 +0200

    updated homepage

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,6 +3,10 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home.routing.module';
 import { AppLayoutModule } from '../layout/app.layout.module';
+import { ChartModule } from 'primeng/chart';
+import { TableModule } from 'primeng/table';
+import { HttpClientModule } from '@angular/common/http'; // Import HttpClientModule
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
@@ -12,7 +16,10 @@ import { AppLayoutModule } from '../layout/app.layout.module';
   imports: [
     CommonModule,
     HomeRoutingModule,
-    AppLayoutModule
+    AppLayoutModule,
+    ChartModule,
+    TableModule,
+    HttpClientModule
   ]
 })
 export class HomeModule {}
---
+++ src/app/models/Athlete.ts
@@ -0,0 +1,6 @@
+export class Athlete {
+    constructor(
+        public id: number,
+        public resource_state: number
+    ) {}
+}
