7b95fb4f529890112a4f426112f0ae6bf79a4627
Jos <jpkotze148@gmail.com>
2024-07-11 19:12:04 +0200

    idk

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,11 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home.routing.module';
-import { AppLayoutModule } from '../layout/app.layout.module';
 import { ChartModule } from 'primeng/chart';
 import { TableModule } from 'primeng/table';
 import { HttpClientModule } from '@angular/common/http'; // Import HttpClientModule
-import { RouterModule } from '@angular/router';
 
 
 @NgModule({
@@ -16,7 +14,6 @@ import { RouterModule } from '@angular/router';
   imports: [
     CommonModule,
     HomeRoutingModule,
-    AppLayoutModule,
     ChartModule,
     TableModule,
     HttpClientModule
